69885338b349dd92fcbdb5e24b194e6c6e7c8573
blqies20 <balqis1maharani@gmail.com>
2025-04-20 17:26:23 +0700

    membuat object struct dalam array dengan getline

--- struct_getline.cpp
+++ struct_getline.cpp
@@ -14,4 +14,24 @@ struct Mahasiswa
     DetailAlamat alamat;
 };
 
-int main(){}
+int main()
+{
+    Mahasiswa mhs[2];
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "Masukkan NIM = ";
+        cin >> mhs[i].nim;
+        cin.ignore();
+
+        cout << "Masukkan Nama = ";
+        getline(cin, mhs[i].nama);
+        
+        cout << "Masukkan Kota = ";
+        cin >> mhs[i].alamat.kota;
+        cout << "Masukkan Provinsi = ";
+        cin >> mhs[i].alamat.provinsi;
+        cin.ignore();
+    }
+    
+}
