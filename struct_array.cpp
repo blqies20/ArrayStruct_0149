524f905593ac4d51d4c7b7ad7723fcf3104f707b
blqies20 <balqis1maharani@gmail.com>
2025-04-20 16:32:32 +0700

    membuat object struct dalam array

--- struct_array.cpp
+++ struct_array.cpp
@@ -8,4 +8,18 @@ struct Mahasiswa
     string alamat;
 };
 
-int main(){}
+int main()
+{
+    Mahasiswa mhs[4];
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Mahasiswa ke-4" << i + 1 << endl;
+        cout << "Masukkan NIM = ";
+        cin >> mhs[i].nim;
+        cout << "Masukkan Nama = ";
+        cin >> mhs[i].nama;
+        cout << "Masukkan Alamat = ";
+        cin >> mhs[i].alamat;
+    }
+    
+}
